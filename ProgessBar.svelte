9909082369e4da0fe250c25f5e716155b1c8e0b1
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-04-08 18:30:15 +0530

    Add ProgressBar component

---
+++ ProgessBar.svelte
@@ -0,0 +1,29 @@
+<!-- Copyright 2025 Ibrahim Ansari -->
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+
+<script lang="ts">
+  import type { SvelteHTMLElements } from 'svelte/elements'
+
+  type Props = SvelteHTMLElements['div'] & { percentage?: number; color?: string }
+  const { percentage, color, ...rest }: Props = $props()
+
+  const width = $derived(`${percentage ?? 0}%`)
+</script>
+
+<div class:progress-bar={true} {...rest}>
+  <div class="progress-bar-filling" style:width style:background-color={color}></div>
+</div>
+
+<style lang="scss">
+  .progress-bar {
+    flex: 1;
+    height: 8px;
+    background-color: grey;
+    border-radius: 4px;
+    display: flex;
+  }
+
+  .progress-bar-filling {
+    border-radius: inherit;
+  }
+</style>
